0e4806bb25acd8674aed8a40fab0133ea1956f05
Nesma abdelfatah <jollynesma123@gmail.com>
2026-07-20 17:52:21 -0700

    Update my  portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Nesma_abdElfatah/', // غيري portfolio لاسم الريبو عندك
+  base: '/', 
 
   plugins: [react(), tailwindcss()],
 
